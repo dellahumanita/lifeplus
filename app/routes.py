82f0083d21097caf3bed3b8e6731a18c5a7ef7e9
Della <humanita.1999@gmail.com>
2020-06-01 01:08:46 -0600

    started from scratch. Finished designing welcome page. Nav still unusable.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app 
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return 'Hello, world!'
+    user = {'username' : 'Della'}
+    return render_template('index.html', title='Home', user=user)
