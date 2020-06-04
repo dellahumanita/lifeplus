ce8de24d8427614778e91d441fed7f538e2301d9
Della <humanita.1999@gmail.com>
2020-06-04 16:35:25 -0600

    registration functionality complete!

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app 
-from app.forms import LoginForm
+from app import app, db 
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User 
 from werkzeug.urls import url_parse
@@ -53,3 +53,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
