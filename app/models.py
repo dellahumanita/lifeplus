8ab78ce6dc1f6db4270121e3657ba821ff7fa2be
Della <humanita.1999@gmail.com>
2020-06-18 17:45:53 -0600

    error in db persists. cannot update db

--- app/models.py
+++ app/models.py
@@ -4,14 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
     email = db.Column(db.String(90), primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     ## Relationships
-    systems = db.relationship('System', backref='user', lazy=True)
-    trackers = db.relationship('Tracker', backref='user', lazy=True)
+    systems = db.relationship('System', backref='creator', lazy='dynamic')
+    # tracker = db.relationship('Habit', backref='tracker', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,46 +23,31 @@ class User(UserMixin, db.Model):
 
 
 class System(db.Model):
-    __tablename__: 'systems'
     sid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True, unique=True)
     descr = db.Column(db.String(120), index=True, unique=True)
+    creator = db.Column(db.String(90), db.ForeignKey('user.email'), nullable=False)
 
     ## Relationships
     habits = db.relationship('Habit', backref='system', lazy=True)
 
-    ## Foreign keys as columns 
-    creator = db.Column(db.String(90), db.ForeignKey('user.email'), nullable=False)
 
     def __repr__(self):
         return '<System {}>'.format(self.sid)
 
 
 class Habit(db.Model):
-    __tablename__ = 'habits'
     hid = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64), index=True, unique=True)
-    progress = db.Column(db.Integer, index=True, unique=True)
-
-    ## Relationships
-    trackers = db.relationship('Tracker', backref='tracker', lazy=True)
+    title = db.Column(db.String(64), index=True)
+    progress = db.Column(db.Integer, index=True)
+    goal = db.Column(db.Integer, index=True)
 
     ## Foreign keys as columns 
     sid = db.Column(db.Integer, db.ForeignKey('system.sid'), nullable=False)
+    # tracker = db.Column(db.String(90), db.ForeignKey('user.email'), nullable=False)
 
     def __repr__(self):
-        return '<Habit {}>'.format(self.hid)
-
-
-class Tracker(db.Model):
-    __tablename__ = 'trackers'
-    hid = db.Column(db.Integer, db.ForeignKey('habits.hid'),
-                    nullable=False, primary_key=True)
-    tracking_by = db.Column(db.String(90), db.ForeignKey('user.email'),
-                            nullable=False)
-
-    def __repr__(self):
-        return '<Habit {}; by User {}>'.format(self.hid, self.tracking_by)
+        return '<Habit #{} for System #{} by User {}>'.format(self.hid, self.sid, self.tracker)
 
     
 
--- lifeplus.py
+++ lifeplus.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import User, System, Habit, Tracker
+from app.models import User, System, Habit
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 
-        'System': System, 'Habit': Habit, 'Tracker': Tracker}
+        'System': System, 'Habit': Habit}
