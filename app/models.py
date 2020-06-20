1b2693d7694d8091e43474ea1a2d9ac2e3329ed7
Della <humanita.1999@gmail.com>
2020-06-20 12:32:37 -0600

    just updating

--- app/models.py
+++ app/models.py
@@ -4,10 +4,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
+    '''This represents a user on the site'''
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    systems = db.relationship('System', backref='creator', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -17,6 +21,17 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class System(db.Model):
+    '''This represents a system that a user has created to store their habits'''
+
+    sid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True)
+    descr = db.Column(db.String(120), index=True)
+    user_id = db.Column(db.String(90), db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<System {}>'.format(self.sid)
 
 @login.user_loader 
 def load_user(id):
