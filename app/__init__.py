252abf829debe477480fc676d8438da905d443ea
Della <humanita.1999@gmail.com>
2020-08-17 20:18:01 -0600

    started following heroku flask deployment tutorial

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__) 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -12,6 +13,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'  #forces users to login before they can
                             # view certain pages of the application 
-Bootstrap(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models 
